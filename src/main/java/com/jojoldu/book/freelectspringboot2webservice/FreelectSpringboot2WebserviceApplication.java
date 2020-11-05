c580f85898bf7186d215ec6c36140d81f8a73874
brant <brant.kj@movill.net>
2020-11-05 10:53:40 +0900

    feat: init 초기 프로젝트 설정

---
+++ src/main/java/com/jojoldu/book/freelectspringboot2webservice/FreelectSpringboot2WebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelectspringboot2webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelectSpringboot2WebserviceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FreelectSpringboot2WebserviceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/jojoldu/book/freelectspringboot2webservice/FreelectSpringboot2WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelectspringboot2webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelectSpringboot2WebserviceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
